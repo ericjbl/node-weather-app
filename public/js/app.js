12db0ec72a213cff521ed972719f09eadcab910b
ericjbl <ej_4176@hotmail.com>
2021-06-28 14:59:52 -0400

    Set up for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     forecastMessage.textContent = 'Loading...'
     errorMessage.textContent = ''
-    fetch('http://localhost:3000/weather?address='+search.value).then((response) => {
+    fetch('/weather?address='+search.value).then((response) => {
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error)
