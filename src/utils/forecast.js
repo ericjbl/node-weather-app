32ec335d0d57acb169ddbc25f7dc56431c64be98
ericjbl <ej_4176@hotmail.com>
2021-06-28 16:31:45 -0400

    Add about description and humidity in the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
             console.log('Unable to find location.')
             callback('Unable to find location.',undefined)
         }else{
-        callback(undefined,body.current.weather_descriptions[0]+ ".It is currently " + body.current.temperature + " degree out, but it feels like " + body.current.feelslike+"\n")
+        callback(undefined,body.current.weather_descriptions[0]+ ".It is currently " + body.current.temperature + " degree out, but it feels like " + body.current.feelslike+".The humidity is "+body.current.humidity+".")
         }
     })
 }
